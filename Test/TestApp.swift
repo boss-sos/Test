e69d44df3d2fd6884f7bc1ccdc611363312d9ca7
sevenxu <sevenxu@tencent.com>
2022-02-25 12:14:20 +0800

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by 徐斌 on 2022/2/25.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
